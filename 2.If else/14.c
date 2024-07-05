d8e9b72c5be3a5d41fb941f86a395a6a748fa737
Mehedi <hasanmehedi26696@gmail.com>
2024-07-05 20:05:44 +0600

    file

--- 2.If else/14.c
+++ 2.If else/14.c
@@ -5,7 +5,7 @@ points fall on one straight line.*/
 #include<stdio.h>
 int main()
 {
-     double x1,y1,x2,y2,x3,y3;
+    double x1,y1,x2,y2,x3,y3;
     double m1,m2;
     scanf("%lf %lf %lf %lf %lf %lf",&x1,&y1,&x2,&y2,&x3,&y3);
     m1=(y2-y1)/(x2-x1);
